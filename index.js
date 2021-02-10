bc63c4f9fa9b1c0976c1c6c3a3f3696aa7b97993
Daniel Moran <damg75cd@gmail.com>
2021-02-10 17:02:08 -0400

    modal 1 updated js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-$(function (){
-    $('body').scrollspy({target: "#navbar", offset: 100});  
-    $('[data-bs-toggle="tooltip"]').tooltip()
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navbar',
+})
+  
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
 })
